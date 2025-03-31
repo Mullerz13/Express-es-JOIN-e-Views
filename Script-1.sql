8bfeedbc8491c9629c19dc7635cfac1fc49b5c1c
Mullerz13 <ryanomuller03@gmail.com>
2025-03-30 21:10:39 -0300

    Update Script-1.sql

--- Script-1.sql
+++ Script-1.sql
@@ -1,7 +1,7 @@
-select * from student s join takes t on s.id = t.id;
 /*seleciona o nome dos alunos, o nome do curso, o ano em que foi feito, o semestre e a nota alcançada, a união é feita 
 com base no id dos alunos que realizaram determinado do curso e a tabela exibida está ordenada pelo ano de maneira ascendente
 isto é, do menor para o maior */
+
 select name, dept_name, year, semester, grade from student s join takes t on s.id = t.id order by year asc;
 
 /*seleciona o id do aluno, seu nome e a quantidade de cursos que realiza, nomeando-a como "Quantidade de cursos"
@@ -9,6 +9,7 @@ select name, dept_name, year, semester, grade from student s join takes t on s.i
  a seleção se limita aos registros onde o nome do departamento é Civil Eng.
  Os registros são agrupados pelo id e nome (todos os registros cujo id e nome se equivalem)
  e por fim são ordenados pela quantidade de cursos do maior para o menor por meio do comando desc*/
+
 select s.id, name, count(course_id) as Quantidade_de_cursos
 from student s 
 join takes t on s.id = t.id 
@@ -17,11 +18,15 @@ group by s.id, name
 order by  Quantidade_de_cursos desc; 
 
 /*Criação da visão civil_eng_students segue o mesmo conceito da explicação anterior
- * seleciona-se o id do aluno, o nome e a quantidade de cursos que fazem  */
+seleciona-se o id do aluno, o nome e a quantidade de cursos que os alunos de Engenharia civil fazem
+e os ordena por id e nome*/
+
 create view civil_eng_students as select s.id, name, count(course_id) as Qtd_cursos
 from student s 
 join takes t on s.id = t.id 
 where dept_name = 'Civil Eng.' 
 group by s.id, name;
+
+/*mostra os elementos da visão criada anteriormente */
 
 select * from civil_eng_students;
