57ff339420891778bf670a648bbc356ce424f2bc
Mullerz13 <ryanomuller03@gmail.com>
2025-03-30 21:12:28 -0300

    Update Script-1.sql

--- Script-1.sql
+++ Script-1.sql
@@ -1,8 +1,13 @@
+/*Questão 01. Crie uma relação a partir da união das tabelas student e takes.*/
+
 /*seleciona o nome dos alunos, o nome do curso, o ano em que foi feito, o semestre e a nota alcançada, a união é feita 
 com base no id dos alunos que realizaram determinado do curso e a tabela exibida está ordenada pelo ano de maneira ascendente
 isto é, do menor para o maior */
 
 select name, dept_name, year, semester, grade from student s join takes t on s.id = t.id order by year asc;
+
+/*Questão 2. Contar a quantidade de cursos realizados pelos alunos do departamento de Civil Eng. 
+Ordenar de maneira descendente a quantidade de cursos associada aos alunos.*/
 
 /*seleciona o id do aluno, seu nome e a quantidade de cursos que realiza, nomeando-a como "Quantidade de cursos"
  especifica o alvo da seleção, nesse caso a tabela student unida a tabela takes de maneira em que o id do estudante seja igual o presente na tabela takes
@@ -17,6 +22,7 @@ where dept_name = 'Civil Eng.'
 group by s.id, name
 order by  Quantidade_de_cursos desc; 
 
+/*Questão 3. Criar uma view chamada 'civil_eng_students' a partir da relação construída na Questão 2.*/
 /*Criação da visão civil_eng_students segue o mesmo conceito da explicação anterior
 seleciona-se o id do aluno, o nome e a quantidade de cursos que os alunos de Engenharia civil fazem
 e os ordena por id e nome*/
